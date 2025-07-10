641a1109ec97c44724c7ee20a0c9d92f7967740f
Robert0309 <jro0309@gmail.com>
2025-07-10 17:21:15 +0200

    added a new challenge :3

--- localization/default.lua
+++ localization/default.lua
@@ -2,11 +2,15 @@ return {
     misc = {
         challenge_names = {
             c_tc_aaaa = "AAAA",
+            c_tc_empty_orbit = "Empty Orbit",
            
             c_tc_aaaa_fu = "AAAA (but fuck you)",
         },
         v_text = {
-            ch_c_by_lazyturtle33 = {"by LazyTurtle33"}
+            ch_c_by_lazyturtle33 = { "By {C:blue}LazyTurtle33{}" },
+            ch_c_no_planets = { "{C:attention}Planet cards{} no longer {C:red}Exist{}" },
+            ch_c_reach_x_ante = { "Reach Ante {C:attention}#1#{} to {C:attention}win" },
+            ch_c_blind_size_multi = {"{C:red}X#1#{} base Blind size"}
         },
     }
 }
--- turtles_challenges.lua
+++ turtles_challenges.lua
@@ -1,9 +1,41 @@
-
+--[[
 SMODS.Challenge({
+	key = "",
+    rules = {
+        custom = {
+            
+        },
+        modifiers = {
+            --{id = 'dollars', value = 4},
+            --{id = 'discards', value = 3},
+            --{id = 'hands', value = 4},
+            --{id = 'hand_size', value = 8},
+            --{id = 'joker_slots', value = 5},
+            --{id = 'consumable_slots', value = 2},
+        },
+    },
+    restrictions = {
+        banned_cards = {
+        },
+        banned_tags = {
+        },
+        banned_other = {
+        },
+    },
+    deck = {
+    },
+	unlocked = function()
+		return true
+	end
+    
+})
+]]
+
+SMODS.Challenge({ -- AAAA
 	key = "aaaa",
     rules = {
         custom = {
-            {id="by_lazyturtle33"}
+            {id="by_lazyturtle33"},
         },
         modifiers = {
             {id = 'discards', value = 3},
@@ -38,12 +70,15 @@ SMODS.Challenge({
             { id = 'j_trading' },
             {id = 'p_standard_normal_1', ids = {
                 'p_standard_normal_1','p_standard_normal_2','p_standard_normal_3','p_standard_normal_4','p_standard_jumbo_1','p_standard_jumbo_2','p_standard_mega_1','p_standard_mega_2',
-            }},
+                }
+            },
+        },
         banned_tags = {
+            {id="tag_standard"},
         },
         banned_other = {
                 { id = 'bl_psychic', type = 'blind' },
-        }},
+        },
     },
     deck = {
         type = 'Challenge Deck',
@@ -54,11 +89,11 @@ SMODS.Challenge({
 	end
     
 })
-SMODS.Challenge({
+SMODS.Challenge({ -- AAAA but fuck you
 	key = "aaaa_fu",
     rules = {
         custom = {
-            {id="by_lazyturtle33"}
+            {id="by_lazyturtle33"},
         },
         modifiers = {
         {id = 'dollars', value = 1},
@@ -104,11 +139,14 @@ SMODS.Challenge({
             {id = 'j_cavendish'},
             {id = 'p_standard_normal_1', ids = {
                 'p_standard_normal_1','p_standard_normal_2','p_standard_normal_3','p_standard_normal_4','p_standard_jumbo_1','p_standard_jumbo_2','p_standard_mega_1','p_standard_mega_2',
-            }},
+                }
+            },
+        },
         banned_tags = {
+            {id="tag_standard"},
         },
         banned_other = {
-        }},
+        },
     },
     deck = {
         type = 'Challenge Deck',
@@ -119,3 +157,120 @@ SMODS.Challenge({
 	end
     
 })
+
+SMODS.Challenge({
+	key = "empty_orbit",
+    rules = {
+        custom = {
+            { id = "no_planets" },
+            { id = "by_lazyturtle33" },
+        },
+        modifiers = {
+            --{id = 'dollars', value = 4},
+            -- {id = 'discards', value = 0},
+            -- {id = 'hands', value = 6},
+            --{id = 'hand_size', value = 8},
+            --{id = 'joker_slots', value = 5},
+            --{id = 'consumable_slots', value = 2},
+        },
+    },
+    restrictions = {
+        banned_cards = {
+            {id='v_planet_merchant'},
+            {id='j_space'},
+            {id='j_burnt'},
+            {id='c_high_priestess'},
+            {id = 'p_celestial_normal_1', ids = {
+                'p_celestial_normal_1','p_celestial_normal_2','p_celestial_normal_3','p_celestial_normal_4',
+                'p_celestial_jumbo_1', 'p_celestial_jumbo_2', 'p_celestial_mega_1', 'p_celestial_mega_2',
+            }},
+        },
+        banned_tags = {
+            {id="tag_meteor"},
+            {id="tag_orbital"},
+        },
+        banned_other = {
+        },
+    },
+    deck = {
+    },
+	unlocked = function()
+		return true
+	end
+    
+})
+
+--[[ 
+SMODS.Challenge({
+	key = "nope",
+    rules = {
+        custom = {
+            {id="no_action",value = 1},
+        },
+        modifiers = {
+        
+        },
+    },
+    restrictions = {
+        banned_cards = {
+        },
+        banned_tags = {
+        },
+        banned_other = {
+        },
+    },
+    deck = {
+    },
+	unlocked = function()
+		return true
+	end
+    
+})
+]]
+
+local game_start_run_ref = Game.start_run
+function Game:start_run(args)
+    game_start_run_ref(self, args)
+    local saveTable = args.savetext or nil
+    if not saveTable then
+        if args.challenge then
+            local _ch = args.challenge
+            if _ch.rules then
+                if _ch.rules.custom then
+                    for k, v in ipairs(_ch.rules.custom) do
+                        if v.id == 'blind_size_multi' then
+                            G.GAME.starting_params.ante_scaling = v.value
+                        elseif v.id == 'reach_x_ante' then
+                            G.GAME.win_ante = v.value
+                        elseif v.id == 'no_planets' then
+                            G.GAME.planet_rate = 0
+                        elseif v.id == 'ante_scaling_2x' then
+                                G.GAME.modifiers.scaling = 2
+                        -- elseif v.id == 'no_action' then
+                        --     if G.GAME.modifiers.flipped_cards and to == G.hand then
+                        --         if pseudorandom(pseudoseed('no_action')) < 1/v.value then
+                        --             G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+                        --                 attention_text({
+                        --                     text = localize('k_nope_ex'),
+                        --                     scale = 1.3, 
+                        --                     hold = 1.4,
+                        --                     major = used_tarot,
+                        --                     backdrop_colour = G.C.SECONDARY_SET.Tarot,
+                        --                     align = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK) and 'tm' or 'cm',
+                        --                     offset = {x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK) and -0.2 or 0},
+                        --                     silent = true
+                        --                     })
+                        --                     G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.06*G.SETTINGS.GAMESPEED, blockable = false, blocking = false, func = function()
+                        --                         play_sound('tarot2', 0.76, 0.4);return true end}))
+                        --                     play_sound('tarot2', 1, 0.4)
+                        --                     used_tarot:juice_up(0.3, 0.5)
+                        --             return true end }))
+                        --         end
+                        --     end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
